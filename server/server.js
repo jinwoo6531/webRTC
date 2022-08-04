e417790aec627b748ad8c102f313ea6d3e7f44b9
jinwoo6531 <jintakk6531@gmail.com>
2022-08-04 17:46:38 +0900

    8/4 simple-peer 확인

--- server/server.js
+++ server/server.js
@@ -50,6 +50,18 @@ io.on('connection', (socket) => {
   socket.on('join-room', (data) => {
     joinRoomHandler(data, socket);
   });
+
+  socket.on('disconnect', () => {
+    disconnectHandler(socket);
+  });
+
+  socket.on('conn-signal', (data) => {
+    signalingHandler(data, socket);
+  });
+
+  socket.on('conn-init', (data) => {
+    initializeConnectionHandler(data, socket);
+  });
 });
 
 //socket.io handlers
@@ -100,10 +112,6 @@ const joinRoomHandler = (data, socket) => {
   };
 
   //join room as user which just is trying to join room passing room id
-  console.log(
-    '머',
-    rooms.find((room) => room.id === roomId)
-  );
   const room = rooms.find((room) => room.id === roomId);
 
   room.connectedUsers = [...room.connectedUsers, newUser];
@@ -114,7 +122,59 @@ const joinRoomHandler = (data, socket) => {
   //add new user to connected users array
   connectedUsers = [...connectedUsers, newUser];
 
+  //peer 연결을 준비하기 위해 이미 이 방에 있는 모든 사용자에게 전송
+  room.connectedUsers.forEach((user) => {
+    if (user.socketId !== socket.id) {
+      const data = {
+        connUserSocketId: socket.id,
+      };
+
+      io.to(user.socketId).emit('conn-prepare', data);
+    }
+  });
+
   io.to(roomId).emit('room-update', { connectedUsers: room.connectedUsers });
+};
+
+const disconnectHandler = (socket) => {
+  //find if user has been registered - if yes remove him from room and connected users array
+  const user = connectedUsers.find((user) => user.socketId === socket.id);
+
+  if (user) {
+    //remove user
+    const room = rooms.find((room) => room.id === user.roomId);
+
+    room.connectedUsers = room.connectedUsers.filter(
+      (user) => user.socketId !== socket.id
+    );
+
+    //leave socket io room
+    socket.leave(user.roomId);
+
+    //TODO
+    // 방을 떠난 나머지 사용자에게 이벤트를 내보냅니다.
+    //방에 머무를 사용자 수가 0인 경우 방을 닫습니다.
+    if (room.connectedUsers.length > 0) {
+      io.to(room.id).emit('room-update', {
+        connectedUsers: room.connectedUsers,
+      });
+    } else {
+      rooms = rooms.filter((r) => r.id !== room.id);
+    }
+  }
+};
+
+const signalingHandler = (data, socket) => {
+  const { connUserSocketId, signal } = data;
+  const signalingData = { signal, connUserSocketId: socket.id };
+  io.to(connUserSocketId).emit('conn-signal', signalingData);
+};
+
+//이미 방에 있는 클라이언트로부터 들어오는 연결을 위해 미리 준비한 정보
+const initializeConnectionHandler = (data, socket) => {
+  const { connUserSocketId } = data;
+  const initData = { connUserSocketId: socket.id };
+  io.to(connUserSocketId).emit('conn-init', initData);
 };
 
 server.listen(PORT, () => {
